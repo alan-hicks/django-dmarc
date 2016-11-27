4ad8a9b34e894ac12e0383bff5d1e278da145fbb
Alan Hicks <ahicks@p-o.co.uk>
2016-11-27 14:08:14 +0000

    Add AppConfig

---
+++ dmarc/apps.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DmarcConfig(AppConfig):
+    name = 'dmarc'
+    verbose_name = "DMARC feedback report manager"
