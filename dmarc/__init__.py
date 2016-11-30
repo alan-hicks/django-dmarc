74138a2cd1c27a1e51ed6561f796328dfae90bb6
Alan Hicks <ahicks@p-o.co.uk>
2016-11-30 13:23:05 +0000

    Version 0.4.7

--- dmarc/__init__.py
+++ dmarc/__init__.py
@@ -9,7 +9,7 @@ DMARC models for managing Aggregate Reports
 http://dmarc.org/resources/specification/
 """
 
-__version__ = '0.4.6'
+__version__ = '0.4.7'
 __author__ = 'Alan Hicks'
 
 default_app_config = 'dmarc.apps.DmarcConfig'
